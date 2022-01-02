0d0950f351afd8d51fa59344dbbd2beaf32188bd
RoxanaCapota <73850383+RoxanaCapota@users.noreply.github.com>
2022-01-02 17:39:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function openPage(pageName, elmnt, color) {
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
+  
   // Remove the background color of all tablinks/buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
